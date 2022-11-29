6dbf7963eb39390242e3e1dc85cc016fe6f1e9df
Mason Currie <43713560+mercurrie@users.noreply.github.com>
2022-11-28 22:22:06 -0600

    formatting

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 # Request city name
-print("Welcome to the band name generator. \nWhat is the name of the city you grew up in?")
-city = input()
+print('Welcome to the band name generator.')
+city = input('What is the name of the city you grew up in?')
 
 # Request pet name
-print("What is your pet's name?")
-pet = input()
+pet = input("What is your pet's name?")
 
 # Output combined names
 print(f"Your band name would be {city} {pet}.")
