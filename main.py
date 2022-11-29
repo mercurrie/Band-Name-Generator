ce1a32e05e0e14a779f5788b4781fa64adb64030
Mason C <43713560+mercurrie@users.noreply.github.com>
2022-11-28 21:56:10 -0600

    band name
    input data, output combined data

---
+++ main.py
@@ -0,0 +1,10 @@
+# Request city name
+print("Welcome to the band name generator. \nWhat is the name of the city you grew up in?")
+city = input()
+
+# Request pet name
+print("What is your pet's name?")
+pet = input()
+
+# Output combined names
+print(f"Your band name would be {city} {pet}.")
